3c9dfe3c49300af4d919b870e8c1fb9acdfabdf5
syunsuke-I <odol@ymail.ne.jp>
2023-12-02 00:12:41 +0900

    DBとの接続確認

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	// PostgreSQLへの接続情報
+	const (
+		host     = "postgres"
+		port     = 5432
+		user     = "postgres"
+		password = "password"
+		dbname   = "postgres"
+	)
+
+	// 接続文字列を作成
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// PostgreSQLに接続
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 接続を確認
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("接続失敗:", err)
+	}
+
+	fmt.Println("接続成功!")
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module backend
 
 go 1.19
 
-require github.com/google/uuid v1.4.0 // indirect
+require (
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
